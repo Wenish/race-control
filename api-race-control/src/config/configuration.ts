45dda12995c3219b1e7c04900ff1ce0d4bc51fa1
Wenish <Wenish@users.noreply.github.com>
2021-11-25 02:17:35 +0100

    feat: adding firebase sdk

--- api-race-control/src/config/configuration.ts
+++ api-race-control/src/config/configuration.ts
@@ -1,6 +1,11 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3001,
-    database: {
-        uri: process.env.DATABASE_URI,
-      }
+  port: parseInt(process.env.PORT, 10) || 3001,
+  database: {
+    uri: process.env.DATABASE_URI,
+  },
+  firebase: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }
 });
--- api-race-control/src/main.ts
+++ api-race-control/src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import * as admin from 'firebase-admin';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -8,6 +9,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('port');
+
+  const adminConfig: admin.ServiceAccount = {
+    projectId: configService.get('firebase.projectId'),
+    privateKey: configService.get('firebase.privateKey').replace(/\\n/g, '\n'),
+    clientEmail: configService.get('firebase.clientEmail'),
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
 
   const config = new DocumentBuilder()
     .setTitle('API Race Control')
