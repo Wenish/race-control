8b0769289fd8c2ec5170606b99401e86fad6281c
Wenish <Wenish@users.noreply.github.com>
2021-11-27 20:30:52 +0100

    feat: add stripe

--- api-race-control/src/config/configuration.ts
+++ api-race-control/src/config/configuration.ts
@@ -7,5 +7,8 @@ export default () => ({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  },
+  stripe: {
+    privateKey: process.env.STRIPE_PRIVATE_KEY
   }
 });
